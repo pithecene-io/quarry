de02906d31ed930cc1250581fde4f3aee7075874
Andrew Hu <me@andrewhu.nyc>
2026-02-04 23:42:49 -0500

    chore(release): 🔖 bump version to v0.2.0
    - Update quarry/types/version.go to 0.2.0
    - Update sdk/package.json to 0.2.0

--- quarry/types/version.go
+++ quarry/types/version.go
@@ -5,4 +5,4 @@ package types
 // per the lockstep versioning policy.
 //
 // This version is authoritative. Contract docs must reference this constant.
-const Version = "0.1.0"
+const Version = "0.2.0"
